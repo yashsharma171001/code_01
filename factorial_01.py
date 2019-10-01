c330a0229330e47dae10d1b5f8bd00acc069d15e
paras-aghija <56034167+paras-aghija@users.noreply.github.com>
2019-10-01 23:06:34 +0530

    Update factorial_01.py

--- factorial_01.py
+++ factorial_01.py
@@ -1,9 +1,9 @@
-# recursive statement
+# recursive function
 def factorial(n):
     if n==1 :
         return 1
 
-    f = n* factorial(n-1)
+    f = n* factorial(n-1) # recursive statement
     return f 
 
 n = int(input("Enter a number :"))
