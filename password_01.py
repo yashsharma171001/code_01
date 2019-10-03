c0c870a4e415a1eaa8972b6d3f2a2a584b6c1d60
yashsharma171001 <56086145+yashsharma171001@users.noreply.github.com>
2019-10-03 13:20:59 +0530

    Update password_01.py

--- password_01.py
+++ password_01.py
@@ -3,10 +3,12 @@ a = list()
 b = ['$','#','@','*','%']
 print("Password length 8 :-")
 for i in range(2):
+
     a.append(chr(random.randrange(65,91)))
     a.append(chr(random.randrange(97,123)))
     a.append(chr(random.randrange(48,58)))
     a.append(random.choice(b))
+
 
 random.shuffle(a)
 print(''.join(a))
